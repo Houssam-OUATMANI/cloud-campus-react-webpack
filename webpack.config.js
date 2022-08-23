17df49c26157eb53481db5396f0a5e4c82a73601
Houssam OUATMANI <ouatmanihoussam@gmail.com>
2022-08-23 13:39:34 +0200

    resolution import sans extension js ou jsx

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,9 @@ module.exports = {
         {test : /\.(js|jsx)$/, exclude : /node_modules/, use : ["babel-loader"]}
     ]
   },
+  resolve : {
+    extensions : ["", "js", "jsx"]
+  },
   devServer : {
     static : {directory : path.join(__dirname, "public")},
     port : 9000,
