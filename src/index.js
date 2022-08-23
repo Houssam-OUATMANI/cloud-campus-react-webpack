2c188ecbf3c8585de52352ed85aa44db017085ae
Houssam OUATMANI <ouatmanihoussam@gmail.com>
2022-08-23 12:10:58 +0200

    Configuratin babel loader et babel presets (env=es5, react=jsx)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx";
 const rootElement = document.querySelector("#root");
-
 const root = createRoot(rootElement);
 root.render(<App />);
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,9 @@ module.exports = {
     path: path.join(__dirname, "dist"),
     filename: "bundle[contenthash].js",
   },
-  
+  module : {
+    rules : [
+        {test : /\.(js|jsx)$/, exclude : /node_modules/, use : ["babel-loader"]}
+    ]
+  }
 };
